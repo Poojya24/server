bd14127cfa4bf830676b68a3c7aed69a18e7b677
poojya <poojya@poojyas-MacBook-Air.local>
2026-03-24 15:31:31 +0530

    fixed env variable name

--- src/utils/seeder.js
+++ src/utils/seeder.js
@@ -11,7 +11,7 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected for seeding...');
   } catch (error) {
     console.error(`Error: ${error.message}`);
